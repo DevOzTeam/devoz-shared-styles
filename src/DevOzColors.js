dffbdc00d2e9bca9e2936f29a2bcadd1ca2d3813
danielemonaca <daniele.monaca1@gmail.com>
2024-03-20 12:42:56 +0100

    initial commit

---
+++ src/DevOzColors.js
@@ -0,0 +1,11 @@
+const DevOzColors = {
+    PINK: "#d0c3dd", //Lavender Gray
+    LAVANDER: "#bcc2e1", //Periwinkle Gray
+    LIGHT_GREEN: "#d2dbd2", //Tasman
+    BLACK: "#2d2a26", //Dune // font-color
+    PURPLE: "#a777ac", //Lavender Purple
+    LIGHT_ORANGE: "#f3ceb4", //Mandys Pink
+    ORANGE: "#d96234", //Flame Pea
+};
+
+module.exports = colors;
